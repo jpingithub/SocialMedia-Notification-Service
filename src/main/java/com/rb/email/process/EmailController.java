dbcdf8784356601482446e0198b6b5361dab2252
jpingithub <nallabothulanagarjun0@gmail.com>
2025-02-11 19:49:41 +0530

    11.02.2025 working fine

--- src/main/java/com/rb/email/process/EmailController.java
+++ src/main/java/com/rb/email/process/EmailController.java
@@ -3,6 +3,8 @@ package com.rb.email.process;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,10 +18,10 @@ public class EmailController {
 
     @Autowired private EmailService emailService;
 
-    @PostMapping("/to")
-    public String sendEmail(@RequestParam("to")String toEmail,@RequestParam("content")String content,@RequestParam("subject")String subject){
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestParam("to")String toEmail, @RequestParam("content")String content, @RequestParam("subject")String subject){
         LOGGER.info("Received data in controller : to - {}, content - {}, subject - {}",toEmail,content,subject);
-        return emailService.sendEmail(toEmail,content,subject);
+        return new ResponseEntity<>(emailService.sendEmail(toEmail,content,subject), HttpStatus.OK);
     }
 
 }
